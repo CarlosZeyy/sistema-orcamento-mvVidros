0da3523e303a2fdd00e470eba23b7578aa9e8ed7
CarlosZeyy <carloszeeyy@gmail.com>
2026-06-06 17:56:37 -0300

    Refactor BudgetForm component to improve input structure and error messaging

--- components/BudgetForm.tsx
+++ components/BudgetForm.tsx
@@ -33,24 +33,31 @@ export default function BudgetForm() {
   return (
     <form onSubmit={handleSubmit(onSubmit, onError)} className="text-gray-700">
       <div>
-        <label>Nome:</label>
-        <input {...register("client.name")} placeholder="Nome do cliente"/>
+        <label>Nome: </label>
+        <input {...register("client.name")} placeholder="Nome do cliente" />
         {errors.client?.name?.message && (
           <p style={{ color: "red" }}>{errors.client?.name?.message}</p>
         )}
       </div>
 
       <div>
-        <label>Endereço:</label>
-        <input {...register("client.address")} placeholder="Endereço do cliente"/>
+        <label>Endereço: </label>
+        <input
+          {...register("client.address")}
+          placeholder="Endereço do cliente"
+        />
         {errors.client?.address?.message && (
           <p style={{ color: "red" }}>{errors.client?.address?.message}</p>
         )}
       </div>
 
       <div>
-        <label>Telefone:</label>
-        <input type="tel" {...register("client.tel")} placeholder="Telefone do cliente"/>
+        <label>Telefone: </label>
+        <input
+          type="tel"
+          {...register("client.tel")}
+          placeholder="Telefone do cliente"
+        />
         {errors.client?.tel?.message && (
           <p style={{ color: "red" }}>{errors.client?.tel?.message}</p>
         )}
@@ -59,29 +66,39 @@ export default function BudgetForm() {
       {fields.map((field, index) => (
         <div key={field.id}>
           <div>
-            <input
-              {...register(`items.${index}.productName` as const)}
-              placeholder="Nome do item"
-            />
-            <input
-              {...register(`items.${index}.productValue` as const, {
-                valueAsNumber: true,
-              })}
-              type="number"
-              placeholder="Valor do item"
-            />
-            <input
-              {...register(`items.${index}.productQuantity` as const, {
-                valueAsNumber: true,
-              })}
-              type="number"
-              placeholder="Quantidade de item"
-            />
-            {errors.items?.[index]?.productName && (
-              <p className="red m-0">
-                {errors.items[index].productName?.message}
-              </p>
-            )}
+            <div>
+              <label>Nome do item: </label>
+              <input
+                {...register(`items.${index}.productName` as const)}
+                placeholder="Nome do item"
+              />
+              {errors.items?.[index]?.productName && (
+                <p className="red m-0">
+                  {errors.items[index].productName?.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <label>Valor do item: R$ </label>
+              <input
+                {...register(`items.${index}.productValue` as const, {
+                  valueAsNumber: true,
+                })}
+                type="number"
+                placeholder="Valor do item"
+              />
+            </div>
+            <div>
+              <label>Quantidade de itens: </label>
+              <input
+                {...register(`items.${index}.productQuantity` as const, {
+                  valueAsNumber: true,
+                })}
+                type="number"
+                placeholder="Quantidade de item"
+              />
+            </div>
+
             {errors.items?.[index]?.productValue && (
               <p className="red m-0">
                 {errors.items[index].productValue?.message}
@@ -99,27 +116,29 @@ export default function BudgetForm() {
               className="cursor-pointer"
               onClick={() => remove(index)}
             >
-              Remover
+              Remover item
             </button>
           }
         </div>
       ))}
 
       {
-        <button
-          type="button"
-          className="cursor-pointer"
-          onClick={() =>
-            append({
-              productName: "",
-              productValue: 0,
-              productQuantity: 1,
-              productCategory: [],
-            })
-          }
-        >
-          Adicionar item
-        </button>
+        <div>
+          <button
+            type="button"
+            className="cursor-pointer"
+            onClick={() =>
+              append({
+                productName: "",
+                productValue: 0,
+                productQuantity: 1,
+                productCategory: [],
+              })
+            }
+          >
+            Adicionar item
+          </button>
+        </div>
       }
       <button type="submit" className="cursor-pointer">
         Enviar
