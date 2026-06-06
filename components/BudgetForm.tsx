0f0845dfec3a02a402eb1a1d181c80be02d282a5
CarlosZeyy <carloszeeyy@gmail.com>
2026-06-06 15:57:17 -0300

    Enhance BudgetForm with item management and validation improvements

--- components/BudgetForm.tsx
+++ components/BudgetForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { budgetSchema } from "@/lib/schema";
@@ -10,37 +10,109 @@ type BudgetFormData = z.infer<typeof budgetSchema>;
 export default function BudgetForm() {
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<BudgetFormData>({
     resolver: zodResolver(budgetSchema),
   });
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "items",
+  });
+
   const onSubmit = (data: BudgetFormData) => {
     console.log("Dados válidos: ", data);
-  }
+  };
 
-   return (
+  return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>Nome:</label>
         <input {...register("client.name")} />
-        {errors.client?.name?.message && <p style={{ color: 'red' }}>{errors.client?.name?.message}</p>}
+        {errors.client?.name?.message && (
+          <p style={{ color: "red" }}>{errors.client?.name?.message}</p>
+        )}
       </div>
 
       <div>
         <label>Endereço:</label>
         <input {...register("client.address")} />
-        {errors.client?.address?.message && <p style={{ color: 'red' }}>{errors.client?.address?.message}</p>}
+        {errors.client?.address?.message && (
+          <p style={{ color: "red" }}>{errors.client?.address?.message}</p>
+        )}
       </div>
 
       <div>
         <label>Telefone:</label>
         <input type="tel" {...register("client.tel")} />
-        {errors.client?.tel?.message && <p style={{ color: 'red' }}>{errors.client?.tel?.message}</p>}
+        {errors.client?.tel?.message && (
+          <p style={{ color: "red" }}>{errors.client?.tel?.message}</p>
+        )}
       </div>
 
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <div>
+            <input
+              {...register(`items.${index}.productName` as const, {
+                required: "Nome do item é obrigatório",
+              })}
+              placeholder="Nome do item"
+            />
+            <input
+              {...register(`items.${index}.productValue` as const, {
+                required: "Valor do item é obrigatório",
+              })}
+              placeholder="Valor do item"
+            />
+            <input
+              {...register(`items.${index}.productQuantity` as const, {
+                required: "Quantidade do item é obrigatório",
+              })}
+              placeholder="Quantidade de item"
+            />
+            {errors.items?.[index]?.productName && (
+              <p className="red m-0">
+                {errors.items[index].productName?.message}
+              </p>
+            )}
+            {errors.items?.[index]?.productValue && (
+              <p className="red m-0">
+                {errors.items[index].productValue?.message}
+              </p>
+            )}
+            {errors.items?.[index]?.productQuantity && (
+              <p className="red m-0">
+                {errors.items[index].productQuantity?.message}
+              </p>
+            )}
+          </div>
+          {
+            <button type="button" onClick={() => remove(index)}>
+              Remover
+            </button>
+          }
+        </div>
+      ))}
+
+      {
+        <button
+          type="button"
+          onClick={() =>
+            append({
+              productName: "",
+              productValue: 0,
+              productQuantity: 1,
+              productCategory: [],
+            })
+          }
+        >
+          Adicionar item
+        </button>
+      }
       <button type="submit">Enviar</button>
     </form>
-  ); 
+  );
 }
--- lib/schema.ts
+++ lib/schema.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 export const budgetSchema = z.object({
   client: z.object({
-    name: z.string().min(2, `Minimo de 2 caracteres`),
-    address: z.string().min(2, `Minimo de 2 caracteres`),
-    tel: z.string().min(2, `Minimo de 2 caracteres`),
+    name: z.string().min(2, `Minimo de 2 caracteres`).nonempty(),
+    address: z.string().min(2, `Minimo de 2 caracteres`).nonempty(),
+    tel: z.string().min(2, `Minimo de 2 caracteres`).nonempty(),
   }),
   items: z.array(
     z.object({
-      productName: z.string().min(2, `Minimo de 2 caracteres`),
+      productName: z.string().min(2, `Minimo de 2 caracteres`).nonempty(),
       productValue: z.number().positive("O valor deve ser maior do que zero"),
       productCategory: z.array(z.string()),
       productQuantity: z.number().positive(`O valor deve ser maior do que zero`)
